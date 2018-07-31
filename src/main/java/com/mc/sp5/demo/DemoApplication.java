23cc9fd76b5d44448a95a5952750dde9da785345
liangzuobin <liangzuobin@meican.com>
2018-07-31 17:37:08 +0800

    copy done

--- src/main/java/com/mc/sp5/demo/DemoApplication.java
+++ src/main/java/com/mc/sp5/demo/DemoApplication.java
@@ -1,29 +1,73 @@
 package com.mc.sp5.demo;
 
+import java.beans.ConstructorProperties;
+import java.util.Objects;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
 import reactor.ipc.netty.http.server.HttpServer;
 
 @SpringBootApplication
 public class DemoApplication {
 
-        public static void main(String[] args) throws Exception {
-		RouterFunction<ServerResponse> router = getRouter();
-		HttpHandler handler = RouterFunctions.toHttpHandler(router);
-		HttpServer.create("localhost", 8080).newHandler(new ReactorHttpHandlerAdapter(handler)).block();
-		Thread.currentThread().join();
+        static class Model {
+                private String name;
+                private int age;
+
+                @ConstructorProperties({"name", "age"})
+                Model(String name, int age) {
+                        this.name = name;
+                        this.age = age;
+                }
+
+                public String getName() {
+                        return this.name;
+                }
+
+                public void setName(String name) {
+                        this.name = name;
+                }
+
+                public int getAge() {
+                        return this.age;
+                }
+
+                public void setAge(int age) {
+                        this.age = age;
+                }
+
+                @Override
+                public boolean equals(Object other) {
+                        Model that;
+                        return this == other || (
+					other instanceof Model
+					&& Objects.equals(this.getName(), (that = (Model) other).getName())
+                                        && this.getAge() == that.getAge());
+                }
         }
 
-        private static RouterFunction<ServerResponse> getRouter() {
+        public static void main(String[] args) throws Exception {
+                RouterFunction<ServerResponse> router = getRouter();
+                HttpHandler handler = RouterFunctions.toHttpHandler(router);
+                HttpServer.create("localhost", 8080)
+                                .newHandler(new ReactorHttpHandlerAdapter(handler)).block();
+                Thread.currentThread().join();
+        }
+
+        static RouterFunction<ServerResponse> getRouter() {
                 HandlerFunction<ServerResponse> index = request -> ServerResponse.ok()
-                                .body(Mono.just("hello"), String.class);
-                return RouterFunctions.route(RequestPredicates.GET("/"), index);
+                                .body(BodyInserters.fromObject("hello"));
+                HandlerFunction<ServerResponse> json = request -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(BodyInserters.fromObject(new Model("Josyee", 20)));
+                return RouterFunctions.route(RequestPredicates.GET("/"), index)
+                                .andRoute(RequestPredicates.GET("/json"), json);
         }
 }
--- src/test/java/com/mc/sp5/demo/DemoApplicationTests.java
+++ src/test/java/com/mc/sp5/demo/DemoApplicationTests.java
@@ -1,16 +1,31 @@
 package com.mc.sp5.demo;
 
+import com.mc.sp5.demo.DemoApplication.Model;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+        private static final WebTestClient client =
+                        WebTestClient.bindToRouterFunction(DemoApplication.getRouter()).build();
+
+        @Test
+        public void testIndex() {
+                client.get().uri("/").exchange().expectStatus().is2xxSuccessful()
+                                .expectBody(String.class).isEqualTo("hello");
+        }
+
+        @Test
+        public void testJson() {
+                client.get().uri("/json").exchange().expectStatus().is2xxSuccessful().expectHeader()
+                                .contentType(MediaType.APPLICATION_JSON).expectBody(Model.class)
+                                .isEqualTo(new Model("Josyee", 20));
+        }
 
 }
