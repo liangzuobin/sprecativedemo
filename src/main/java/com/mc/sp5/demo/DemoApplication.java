af5be394ac52f1c106719848178d79e9b2600f18
liangzuobin <liangzuobin@meican.com>
2018-07-31 16:52:30 +0800

    index

--- src/main/java/com/mc/sp5/demo/DemoApplication.java
+++ src/main/java/com/mc/sp5/demo/DemoApplication.java
@@ -1,12 +1,29 @@
 package com.mc.sp5.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+import reactor.ipc.netty.http.server.HttpServer;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+        public static void main(String[] args) throws Exception {
+		RouterFunction<ServerResponse> router = getRouter();
+		HttpHandler handler = RouterFunctions.toHttpHandler(router);
+		HttpServer.create("localhost", 8080).newHandler(new ReactorHttpHandlerAdapter(handler)).block();
+		Thread.currentThread().join();
+        }
+
+        private static RouterFunction<ServerResponse> getRouter() {
+                HandlerFunction<ServerResponse> index = request -> ServerResponse.ok()
+                                .body(Mono.just("hello"), String.class);
+                return RouterFunctions.route(RequestPredicates.GET("/"), index);
+        }
 }
